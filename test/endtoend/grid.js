d2fb617d5822b347392aaf4c03391589f977ea4c
Graeme Foster <graemef@gmail.com>
2012-09-10 17:47:51 +0100

    Use letter producers to populate the grid.

--- test/endtoend/grid.js
+++ test/endtoend/grid.js
@@ -1,11 +1,18 @@
 var serverFactory = require('../../lib/serverFactory');
 var Browser = require('./helpers/Browser');
 
-describe('Given a new server has started', function () {
+describe('Given a new server has started with a fixed grid', function () {
   var server;
 
   beforeEach(function (done) {
-    server = serverFactory({port: 0});
+    server = serverFactory(
+      { port: 0,
+        grid: { width: 32,
+          height: 32,
+          letterProducer: function () {
+            return 'X';
+          }}
+      });
     server.start(done);
   });
 
@@ -19,8 +26,8 @@ describe('Given a new server has started', function () {
 
     describe('the grid', function () {
       it('should contain letters', function () {
-        browser.getLetter(0, 0).should.be.within('A', 'Z');
-        browser.getLetter(31, 31).should.be.within('A', 'Z');
+        browser.getLetter(0, 0).should.equal('X');
+        browser.getLetter(31, 31).should.equal('X');
       });
     });
 
--- test/unit/Grid.js
+++ test/unit/Grid.js
@@ -1,11 +1,14 @@
 var _ = require('underscore');
 var Grid = require('../../lib/Grid');
+var SequentialProducer = require('../../lib/SequentialProducer');
 
-describe('A 20x10 Grid', function () {
+describe('A 5x4 Grid with a sequential letter producer', function () {
   var grid;
 
   beforeEach(function () {
-    grid = new Grid(20, 10);
+    grid = new Grid({width: 5,
+                      height: 4,
+                      letterProducer: new SequentialProducer().next});
   });
 
   describe('when filled', function () {
@@ -17,14 +20,14 @@ describe('A 20x10 Grid', function () {
       grid.getLetter(2, 2).should.be.within('A', 'Z');
     });
 
-    it('should contain 10 rows', function () {
-      grid.rows.should.have.length(10);
+    it('should contain 4 rows', function () {
+      grid.rows.should.have.length(4);
     });
 
     describe('each row', function () {
-      it('should contain 20 letters', function () {
+      it('should contain 5 letters', function () {
         _.each(grid.rows, function (row) {
-          row.should.have.length(20);
+          row.should.have.length(5);
         });
       });
     });
