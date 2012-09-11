de103f3c91719779a60443b1496c34e46b868614
Graeme Foster <graemef@gmail.com>
2012-09-11 13:03:32 +0100

    Fixed global variable.

--- wordburglar.js
+++ wordburglar.js
@@ -1,7 +1,7 @@
 var serverFactory = require('./lib/serverFactory');
 var config = require('./lib/config');
 
-server = serverFactory(config.get('server'));
+var server = serverFactory(config.get('server'));
 
 server.start(function () {
   console.log('Listening on', server.uri());
