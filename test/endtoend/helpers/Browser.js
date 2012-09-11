5573bbd74af7697364642732e1483f6245f90c26
Graeme Foster <graemef@gmail.com>
2012-09-11 13:01:22 +0100

    The browser can now click on the letters.

--- test/endtoend/helpers/Browser.js
+++ test/endtoend/helpers/Browser.js
@@ -9,17 +9,23 @@ Browser.prototype.navigateHome = function (callback) {
   this.zombie.visit(this.uri, callback);
 };
 
-Browser.prototype.hasLogInButton = function () {
-  return this.zombie.document.getElementById('loginButton');
+Browser.prototype.clickLetter = function (letterLoc, callback) {
+  this.zombie.clickLink(createLetterSelector(letterLoc.x, letterLoc.y), callback);
+};
+
+Browser.prototype.mark = function (firstLetter, lastLetter, callback) {
+  var self = this;
+  self.clickLetter(firstLetter, function (err) {
+    if (err) {
+      return callback(err);
+    }
+    return self.clickLetter(lastLetter, callback);
+  });
 };
 
 Browser.prototype.getLetter = function (x, y) {
-  return this.zombie.text('table#grid' +
-                            '> tbody' +
-                            '> tr:nth-child(' + (y + 1) + ')' +
-                            '> td:nth-child(' + (x + 1) + ')');
+  return this.zombie.text(createLetterSelector(x, y));
 };
-
 Browser.prototype.getScore = function () {
   return parseInt(this.zombie.text('span#score'), 10);
 };
@@ -29,3 +35,11 @@ Browser.prototype.getTitle = function () {
 };
 
 module.exports = Browser;
+
+function createLetterSelector(x, y) {
+  return 'table#grid' +
+    '> tbody' +
+    '> tr:nth-child(' + (y + 1) + ')' +
+    '> td:nth-child(' + (x + 1) + ')' +
+    '> a';
+}
