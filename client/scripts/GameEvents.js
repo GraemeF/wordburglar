d5bd39a24f81f7d427dd7b3b2aa1b146dde84cbd
Graeme Foster <graemef@gmail.com>
2012-09-15 17:50:47 +0100

    Fixed global leak.

--- client/scripts/GameEvents.js
+++ client/scripts/GameEvents.js
@@ -1,41 +1,41 @@
-function monitorConnectionState(socket, emitter) {
-  socket.on('connect', function () {
-    emitter.trigger('connection', 'connected');
-  });
-  
-  socket.on('connecting', function (transport_name) {
-    emitter.trigger('connection', 'connecting with ' + transport_name);
-  });
-
-  socket.on('connect_failed', function () {
-    emitter.trigger('connection', 'connection failed');
-  });
-
-  socket.on('close', function () {
-    emitter.trigger('connection', 'closed');
-  });
-
-  socket.on('disconnect', function () {
-    emitter.trigger('connection', 'disconnected');
-  });
-
-  socket.on('reconnect', function (transport_type, reconnectionAttempts) {
-    emitter.trigger('connection', 'reconnected with ' + transport_type + ' after ' + reconnectionAttempts + ' attempts');
-  });
-
-  socket.on('reconnecting', function (reconnectionDelay, reconnectionAttempts) {
-    emitter.trigger('connection', 'reconnecting (' + reconnectionAttempts + ' attempts)');
-  });
-
-  socket.on('reconnect_failed', function () {
-    emitter.trigger('connection', 'reconnect failed');
-  });
-}
-
 define(['underscore',
         'socket.io',
         'backbone'
        ], function (_, io, backbone) {
+
+  function monitorConnectionState(socket, emitter) {
+    socket.on('connect', function () {
+      emitter.trigger('connection', 'connected');
+    });
+
+    socket.on('connecting', function (transport_name) {
+      emitter.trigger('connection', 'connecting with ' + transport_name);
+    });
+
+    socket.on('connect_failed', function () {
+      emitter.trigger('connection', 'connection failed');
+    });
+
+    socket.on('close', function () {
+      emitter.trigger('connection', 'closed');
+    });
+
+    socket.on('disconnect', function () {
+      emitter.trigger('connection', 'disconnected');
+    });
+
+    socket.on('reconnect', function (transport_type, reconnectionAttempts) {
+      emitter.trigger('connection', 'reconnected with ' + transport_type + ' after ' + reconnectionAttempts + ' attempts');
+    });
+
+    socket.on('reconnecting', function (reconnectionDelay, reconnectionAttempts) {
+      emitter.trigger('connection', 'reconnecting (' + reconnectionAttempts + ' attempts)');
+    });
+
+    socket.on('reconnect_failed', function () {
+      emitter.trigger('connection', 'reconnect failed');
+    });
+  }
 
   var GameEvents = function () {
     _.extend(this, backbone.Events);
