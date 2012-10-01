0ecea19661b104e217c6dffc961b9098baaa876b
Graeme Foster <graemef@gmail.com>
2012-10-01 13:05:47 +0100

    Look up players when they connect.

--- test/unit/PlayerRepo.js
+++ test/unit/PlayerRepo.js
@@ -1,9 +1,27 @@
 var PlayerRepo = require('../../lib/PlayerRepo');
 
 describe('PlayerRepo', function () {
-  var test;
+  var players;
 
   beforeEach(function () {
-    test = new PlayerRepo();
+    players = new PlayerRepo();
+  });
+
+  it('should not contain a player with token 1', function () {
+    expect(players.findByToken(1)).to.not.be.ok;
+  });
+
+  describe('when player 1 is added', function () {
+    beforeEach(function () {
+      players.addNew({token: 1, name: 'player 1'});
+    });
+
+    it('should contain a player with token 1', function () {
+      expect(players.findByToken(1)).to.be.ok;
+    });
+
+    it('should not contain a player with token 2', function () {
+      expect(players.findByToken(2)).to.not.be.ok;
+    });
   });
 });
