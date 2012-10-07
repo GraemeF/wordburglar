a77b9bda9c3ccb3e8cee0ccf9c7074b27f4701fd
Graeme Foster <graemef@gmail.com>
2012-10-07 18:59:11 +0100

    Only award points for unused letters.

--- test/unit/Game.js
+++ test/unit/Game.js
@@ -1,11 +1,26 @@
 var Game = require('../../lib/Game');
 var events = require('events');
+var _ = require('underscore');
 
 describe('Game', function () {
   var game;
   var isWord;
   var grid;
   var scoreboard;
+  var playerId = 'player id';
+  const line = 'line';
+
+  function unusedLetters(letters) {
+    return _.map(letters, function (letter) {
+      return {letter: letter, used: false};
+    });
+  }
+
+  function usedLetters(letters) {
+    return _.map(letters, function (letter) {
+      return {letter: letter, used: true};
+    });
+  }
 
   beforeEach(function () {
     scoreboard = new events.EventEmitter();
@@ -19,48 +34,59 @@ describe('Game', function () {
     game = new Game(grid, scoreboard, isWord);
   });
 
-  describe('and the grid has a word at a particular line', function () {
+  describe('and the grid has an unused 5 letter word', function () {
     const line = 'line of HELLO';
 
     beforeEach(function () {
-      grid.getLetters.withArgs(line).returns('HELLO');
+      grid.getLetters.withArgs(line).returns(unusedLetters('HELLO'));
       isWord.withArgs('HELLO').returns(true);
     });
 
-    describe('and a player marks the line', function () {
-      var playerId = 'player id';
+    describe('and a player marks the word', function () {
 
       beforeEach(function () {
         game.markLine(playerId, line);
       });
 
-      it('should award a point', function () {
+      it('should award 5 points', function () {
         scoreboard.awardPoints
           .should.have.been.calledWith(playerId, 5);
       });
+    });
+  });
 
-      it('should mark the line as used', function () {
-        grid.markUsed.should.have.been.calledWith(line);
+  describe('and the grid has a used 5 letter word', function () {
+    beforeEach(function () {
+      grid.getLetters.withArgs(line).returns(usedLetters('HELLO'));
+      isWord.withArgs('HELLO').returns(true);
+    });
+
+    describe('and a player marks the word', function () {
+      beforeEach(function () {
+        game.markLine(playerId, line);
       });
 
-      describe('and the grid has another word at a particular line', function () {
-        const line = 'line of WORLD';
+      it('should not award any points', function () {
+        scoreboard.awardPoints
+          .should.not.have.been.called;
+      });
+    });
+  });
 
-        beforeEach(function () {
-          grid.getLetters.withArgs(line).returns('WORLD');
-          isWord.withArgs('WORLD').returns(true);
-        });
+  describe('and the grid has unused letters that are not a word', function () {
+    beforeEach(function () {
+      grid.getLetters.withArgs(line).returns(unusedLetters('ABC'));
+      isWord.withArgs('ABC').returns(false);
+    });
 
-        describe('and the player marks the other line', function () {
-          beforeEach(function () {
-            game.markLine(playerId, line);
-          });
+    describe('and the player marks the line', function () {
+      beforeEach(function () {
+        game.markLine(playerId, line);
+      });
 
-          it('should award another point', function () {
-            scoreboard.awardPoints
-              .should.have.been.calledTwice;
-          });
-        });
+      it('should not award any points', function () {
+        scoreboard.awardPoints
+          .should.not.have.been.called;
       });
     });
   });
