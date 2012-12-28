68ca8f6ee144cbaef9ba17068a52ad64e0d1becb
Graeme Foster <graemef@gmail.com>
2012-12-28 10:37:28 +0000

    Added a unit test for name form submission.

--- client/src/spec/UI.js
+++ client/src/spec/UI.js
@@ -7,13 +7,38 @@ var soon = require('patience').soon;
 var sinonChai = require("sinon-chai");
 chai.use(sinonChai);
 
-var Game = require('../lib/Game');
 var _ = require('underscore');
 var backbone = require('backbone');
 var sinon = require('sinon');
+require('./jasmine-fixture');
+var UI = require('../lib/UI');
 
-describe('UI x', function() {
-  it('should fail', function(done) {
-    done(new Error('hooray yall:w !'));
+describe('UI', function() {
+  beforeEach(function() {
+    jasmineFixture($)
+  });
+
+  describe('with a name form', function() {
+    beforeEach(function() {
+      affix('form#nameForm > input');
+    });
+
+    describe('when I submit my name', function() {
+      var newName;
+
+      beforeEach(function() {
+        newName = 'not set';
+        var ui = new UI();
+        ui.on('setName', function(data) {
+          newName = data;
+        });
+        $('form#nameForm > input').val('Bob');
+        $('form#nameForm').submit();
+      });
+
+      it('should emit a setName event', function() {
+        newName.should.equal('Bob');
+      });
+    });
   });
 });
